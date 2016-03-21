7761703efd5ce2540c12eda29556717fc3268b89
Joshua Bradt <jbradt26@gmail.com>
2016-03-20 22:02:20 -0400

    Rearranged test cases and added test with regular function.

--- test/test_ParallelMap.cc
+++ test/test_ParallelMap.cc
@@ -1,10 +1,9 @@
-#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "ThreadPool.hh"
 #include <vector>
 #include <numeric>
 
-TEST_CASE("Parallel for with MapRange")
+TEST_CASE("ParallelMap function works with a lambda", "[ParallelMap]")
 {
     size_t N = 1000;
 
@@ -12,9 +11,9 @@ TEST_CASE("Parallel for with MapRange")
     std::vector<double> b (N);
     std::iota(a.begin(), a.end(), 0);
 
-    SECTION("Parallel version works with one thread")
+    SECTION("Parallel version works with four threads")
     {
-        ThreadPool pool(1);
+        ThreadPool pool(4);
         std::function<double(double)> workFxn ([](double v)->double{ return 4*v; });
         pool.ParallelMap(a.begin(), a.end(), b.begin(), workFxn);
 
@@ -22,12 +21,25 @@ TEST_CASE("Parallel for with MapRange")
             REQUIRE(b.at(i) == Approx(4*a.at(i)));
         }
     }
+}
+
+double testFunction(double v)
+{
+    return 4*v;
+}
+
+TEST_CASE("ParallelMap function works with a regular function", "[ParallelMap]")
+{
+    size_t N = 1000;
+
+    std::vector<double> a (N);
+    std::vector<double> b (N);
+    std::iota(a.begin(), a.end(), 0);
 
     SECTION("Parallel version works with four threads")
     {
         ThreadPool pool(4);
-        std::function<double(double)> workFxn ([](double v)->double{ return 4*v; });
-        pool.ParallelMap(a.begin(), a.end(), b.begin(), workFxn);
+        pool.ParallelMap(a.begin(), a.end(), b.begin(), std::function<double(double)>(testFunction));
 
         for (size_t i = 0; i < N; i++) {
             REQUIRE(b.at(i) == Approx(4*a.at(i)));
